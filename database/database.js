b6e86b4516dd1099bc99af360a895c0dd62d8b65
hendrianiyan <100855337+hendrianiyan@users.noreply.github.com>
2023-06-11 22:55:08 +0700

    update index

--- database/database.js
+++ database/database.js
@@ -1,11 +1,24 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql');
 
-// Buat koneksi dengan database
+// // Buat koneksi dengan database
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password: '',
+//   database: 'dbcapstone'
+// });
+
+// // Ekspor objek koneksi untuk digunakan di aplikasi
+// module.exports = connection;
+
+const mysql = require('mysql');
+
+// Buat koneksi ke database
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: '34.101.76.176',
   user: 'root',
-  password: '',
-  database: 'dbcapstone'
+  password: 'dbserver',
+  database: 'capstone'
 });
 
 // Ekspor objek koneksi untuk digunakan di aplikasi
